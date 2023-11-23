c4f7fd09571dde4717943962e5ea79c38d97a630
adib mokhtari <adib.mokhtari@gmail.com>
2023-11-23 19:42:42 +0330

    - build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 
+import Link from "next/link";
+
 import { Button } from "@radix-ui/themes";
 
 const issues = () => {
   return (
     <div>
-      <Button>New Issue</Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
     </div>
   );
 };
