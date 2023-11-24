b00edd113333a1e13ddf5f9470b7c66da3dff7d2
adib mokhtari <adib.mokhtari@gmail.com>
2023-11-24 14:44:03 +0330

    Connect edit page for updating issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -38,7 +38,8 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
   const submitHandler = handleSubmit(async (data) => {
     try {
       setSubmitting(true);
-      await axios.post("/api/issues", data);
+      if (issue) await axios.patch(`/api/issues/${issue.id}`, data);
+      else await axios.post("/api/issues", data);
       router.push("/issues");
     } catch (error) {
       setSubmitting(false);
@@ -74,7 +75,10 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         />
         <ErrorMessage>{errors.description?.message}</ErrorMessage>
 
-        <Button>Submit New Issue {isSubmitting && <Spinner />}</Button>
+        <Button>
+          {issue ? "Update Issue" : "Submit New Issue"}{" "}
+          {isSubmitting && <Spinner />}
+        </Button>
       </form>
     </div>
   );
