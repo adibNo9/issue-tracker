1ffa99e704caa94c96aeffc4ed7cec858a7921cc
adib mokhtari <adib.mokhtari@gmail.com>
2023-11-30 13:32:58 +0330

    update authOption for fixing jwt bug

--- app/auth/authOptions.ts
+++ app/auth/authOptions.ts
@@ -14,6 +14,9 @@ const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 };
 
 export default authOptions;
