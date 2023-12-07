998fbae7e6f404efc2ad27b5b5c05219a3ab1281
adib mokhtari <adib.mokhtari@gmail.com>
2023-12-07 13:44:05 +0330

    update next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "referrer-policy",
+            value: "no-referrer",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
-
 
 // Injected content via Sentry wizard below
 
